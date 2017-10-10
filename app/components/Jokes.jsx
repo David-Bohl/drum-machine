ad46ef1807ead24fc62655dd121099f5e1eb411d
David-Bohl <davebohl@gmail.com>
2017-10-10 15:20:12 -0400

    undo previous changes

--- app/components/Jokes.jsx
+++ app/components/Jokes.jsx
@@ -20,7 +20,6 @@ export default class BonesJokes extends Component {
     const {joke, answered} = this.state
     return (
       <div onClick={answered ? this.nextJoke : this.answer}>
-        <h1>drum machine coming soon!</h1>
         <h1>{joke.q}</h1>
         {answered && <h2>{joke.a}</h2>}
         <cite>~xoxo, bones</cite>
