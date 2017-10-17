e2d7d7315068789d7d4c3192e73d0ae44dd0c9fb
David-Bohl <davebohl@gmail.com>
2017-10-17 17:43:46 -0400

    buttons change colors.  Clean up code

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,35 +1,41 @@
 import React, { Component } from 'react'
 
 import StepButton from './StepButton'
-import SaveButton from './SaveButton'
 
-let saveBtnStyle = {
-  background: "aliceblue",
-  height: "40px",
-  width: "40px",
-  display: "inline-block",
-  margin: "10px"
-};
 
-var stepButtons = [];
-for(var i = 0; i < 16; i++){
-  stepButtons.push(<StepButton isSelecteded={ false } />)
-}
+
+
 
 class Home extends Component {
 
   constructor(props){
     super( props );
     this.saveBeat = this.saveBeat.bind(this);
+    this.stepButtons = [];
+  }
+
+  componentWillMount(){
+    for(var i = 0; i < 16; i++){
+      this.stepButtons.push(<StepButton/>)
+    }
   }
 
   render() {
+
+    let saveBtnStyle = {
+      background: "aliceblue",
+      height: "40px",
+      width: "40px",
+      display: "inline-block",
+      margin: "10px"
+    };
+
 
 
     return (
       <div >
         <h1>drum machine</h1>
-        {stepButtons}
+        {this.stepButtons}
         <span style={saveBtnStyle} onClick={ this.saveBeat }>SAVE</span>
 
       </div>
@@ -38,13 +44,10 @@ class Home extends Component {
 
   saveBeat(){
     alert("beat saved");
-
-    stepButtons.forEach((element) => {
+    this.stepButtons.forEach((element) => {
       console.log(element);
     });
-
   }
-
 
 }
 
--- app/components/StepButton.jsx
+++ app/components/StepButton.jsx
@@ -1,39 +1,39 @@
 import React, { Component } from 'react'
 
-/* we can find a better way to handle CSS later */
-
-let btnColor = "#aae"
-
-let btnStyle = {
-  background: btnColor,
-  height: "40px",
-  width: "40px",
-  display: "inline-block",
-  margin: "10px"
-};
 
 class StepButton extends Component {
 
   constructor ( props ) {
     super( props );
     this.state = {
-      isSelected: null
+      isSelected: false
     }
-
     this._handleChange = this._handleChange.bind(this);
-
   }
 
   componentWillMount () {
     this.setState( { isSelected: this.props.isSelected } );
   }
 
-
-
   render() {
+    {/* we can find a better way to handle styles later */}
+    let btnColor = "#aae";
+
+    if(this.state.isSelected){
+      btnColor = "yellow";
+    }
+
+    let btnStyle = {
+      background: btnColor,
+      height: "40px",
+      width: "40px",
+      display: "inline-block",
+      margin: "10px"
+    };
+
     return (
       <span style={btnStyle} onClick={ this._handleChange }>
-        {this.state.isSelected ? '1' : '0'}
+        {/*this.state.isSelected ? '1' : '0'*/}
       </span>
 
     )
@@ -43,8 +43,6 @@ class StepButton extends Component {
     !this.state.isSelected ? console.log('selected') : console.log('un selected');
     this.setState( { isSelected: !this.state.isSelected } );
   }
-
-
 
 }
 
