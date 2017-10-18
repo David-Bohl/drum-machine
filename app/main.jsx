ea6034c12172e0c182976a96646f5bdac2eea51c
David-Bohl <davebohl@gmail.com>
2017-10-17 22:17:55 -0400

    remove all references to jokes component

--- app/main.jsx
+++ app/main.jsx
@@ -13,7 +13,6 @@ import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
 import store from './store'
-import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
@@ -36,8 +35,8 @@ render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
+        <IndexRedirect to="/home" />
+        <Route path="/home" component={Home} />
       </Route>
       <Route path='*' component={NotFound} />
     </Router>
